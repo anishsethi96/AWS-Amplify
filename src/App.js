affb0994adc800c564aef61a11dd1c6f042b2f8b
anish sethi <anish.sethi96@hotmail.com>
2021-04-20 00:09:46 -0400

    UI update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
-  );
+import AddSPD from "./components/spdData.component";
+
+
+class App extends Component {
+
+  state = { message: "" }
+  callbackFunction = (childData) => {
+        this.setState({message: "User ID - " + childData})
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav className="navbar navbar-expand navbar-dark bg-dark">
+            <a href="/login" className="navbar-brand">
+              CIRCADIAN&reg; Lighting Calculator
+            </a>
+            <div className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/app1"} className="nav-link">
+                  Light Source Measurement
+                </Link>
+              </li>
+            </div>
+          </nav>
+
+          <div className="container mt-3">
+            <Switch>
+              <Route exact path="/app1" component={AddSPD} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
